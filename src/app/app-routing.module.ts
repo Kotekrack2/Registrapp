6d255a1e0c2e7f1b4d205f1521b0d7b6cc354222
Kotekrack2 <joselitodonoso584@gmail.com>
2024-11-06 00:38:03 -0300

    enrutador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,8 +22,9 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
-  },  {
-    path: 'error404',
+  },
+  {
+    path: '**',
     loadChildren: () => import('./error404/error404.module').then( m => m.Error404PageModule)
   },
 
